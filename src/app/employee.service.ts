d1c23e9304aaa446fe2c6a9beae635f2ce4ba737
lamboyjat <lamboyjat@gmail.com>
2021-04-27 02:44:08 +0200

    add the frontend for the employee app manager, component for employee services, Add, Update, and Delete functionality

---
+++ src/app/employee.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Employee } from './employee';
+import { environment } from 'src/environments/environment';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+
+  constructor(private http: HttpClient) { }
+
+  private apiServerUrl = environment.apiBaseUrl; 
+
+    public getEmployees(): Observable<Employee> {
+        return this.http.get<Employee>(`${this.apiServerUrl}/employee/all`);
+    }
+
+    public addEmployee(employee: Employee): Observable<Employee> {
+        return this.http.post<Employee>(`${this.apiServerUrl}/employee/add`, employee);
+    }
+
+    public updateEmployee(employee: Employee): Observable<Employee> {
+        return this.http.put<Employee>(`${this.apiServerUrl}/employee/update`, employee);
+    }
+
+    public deleteEmployee(employee: number): Observable<void> {
+        return this.http.delete<void>(`${this.apiServerUrl}/employee/delete/${employee}`);
+    }
+}
